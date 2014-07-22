3bea7bba9ed4431f2c410582ccdfef80803327c4
Mikael Fors <fors.mikael@gmail.com>
2014-07-23 00:02:51 +0200

    Update trie.cpp

--- src/trees/trie.cpp
+++ src/trees/trie.cpp
@@ -10,12 +10,14 @@
 */
 
 #include "trie.h"
+#include <queue>
 
 namespace project_euler_helper {
 
     /* ######## ------- NODE ------- ######## */
 
     TrieNode::TrieNode(const char* p_string) {
+        m_count = 0;
         if(*p_string != 0) {
             m_children[*p_string] = new TrieNode(p_string + 1);
         }
@@ -28,29 +30,70 @@ namespace project_euler_helper {
         }
     };
 
-    void TrieNode::add(const char* p_string) {
-        if(*p_string != 0) {
-            if(m_children.find(*p_string) != m_children.end()) {
-                m_children[*p_string]->add(p_string + 1);
+    void TrieNode::insert(const char* p_string) {
+        TrieNode *node = this;
+        while(*p_string) {
+            if(node->m_children.find(*p_string) != node->m_children.end()) {
+                node = node->m_children[*p_string];
             } else {
-                m_children[*p_string] = new TrieNode;
-                m_children[*p_string]->add(p_string + 1);
+                node->m_children[*p_string] = new TrieNode;
+                node = node->m_children[*p_string];
             }
-        } else {
-            m_count++;
+            ++p_string;
         }
+        node->m_count++;
     }
 
     PrefixSearchResult TrieNode::find(const char* p_string) {
-        if(*p_string != 0) {
-            if(m_children.find(*p_string) != m_children.end()) {
-                return m_children[*p_string]->find(p_string + 1);
+        TrieNode* node = this;
+        while(*p_string) {
+            if(node->m_children.find(*p_string) != node->m_children.end()) {
+                node = node->m_children[*p_string];
             } else {
-                return (PrefixSearchResult){ 0, 0 };
+                (PrefixSearchResult){0, 0};
             }
-        } else {
-            return (PrefixSearchResult){ 1, m_count > 0 };
+            ++p_string;
         }
+        return (PrefixSearchResult){1, node->m_count > 0};
+    }
+
+    std::vector<std::string> TrieNode::contentWithPrefix(const char* p_prefix) {
+
+        TrieNode* node = this;
+        std::vector<std::string> result{};
+        std::string prefix{""};
+
+        while(*p_prefix) {
+            if(node->m_children.find(*p_prefix) != node->m_children.end()) {
+                node = node->m_children[*p_prefix];
+                prefix.push_back(*p_prefix);
+                ++p_prefix;
+            } else {
+                return result;
+            }
+        }
+
+        // If we are here, we found something and need to BFS
+        std::queue<PrefixQueueItem> jobs{};
+        jobs.push((PrefixQueueItem){node, prefix});
+        PrefixQueueItem currentJob;
+
+        while(!jobs.empty()) {
+            currentJob = jobs.front();
+            jobs.pop();
+            if(currentJob.node && currentJob.node->m_count)
+                result.push_back(currentJob.key);
+            for(std::map<char,TrieNode*>::iterator child = currentJob.node->m_children.begin();
+                child != currentJob.node->m_children.end(); ++child) {
+
+                jobs.push((PrefixQueueItem){
+                    child->second,
+                    currentJob.key + std::string(1, child->first)
+                });
+            }
+        }
+
+        return result;
     }
 
 
@@ -60,7 +103,7 @@ namespace project_euler_helper {
         if(p_strings.size()) {
             m_root = new TrieNode(p_strings[0].c_str());
             for(uint i = 1; i < p_strings.size(); ++i) {
-                m_root->add(p_strings[i].c_str());
+                m_root->insert(p_strings[i].c_str());
             }
         } else {
             m_root = new TrieNode;
@@ -71,9 +114,9 @@ namespace project_euler_helper {
         delete m_root;
     }
 
-    void Trie::add(const std::string& p_string) {
+    void Trie::insert(const std::string& p_string) {
         if(p_string.length())
-            m_root->add(p_string.c_str());
+            m_root->insert(p_string.c_str());
     }
 
     bool Trie::contains(const std::string& p_string) {
@@ -83,5 +126,9 @@ namespace project_euler_helper {
 
     bool Trie::containsPrefix(const std::string& p_prefix) {
         return p_prefix.length() ? m_root -> find(p_prefix.c_str()).found: 1;
+    }
+
+    std::vector<std::string> Trie::contentWithPrefix(const char* p_prefix) {
+        return m_root->contentWithPrefix(p_prefix);
     }
 }
